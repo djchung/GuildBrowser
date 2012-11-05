9eeef4b43f107e025cb8fa87e99216724b257abd
DJ Chung <djchung3@gmail.com>
2012-11-05 10:31:19 -0800

    test thing thing

--- GuildBrowserLogicTests/CharacterTest.m
+++ GuildBrowserLogicTests/CharacterTest.m
@@ -63,6 +63,7 @@
     STAssertEqualObjects(_testGuy.gender, @"Male", @"gener is wrong");
     STAssertEqualObjects(_testGuy.averageItemLevel, @379, @"avg item level is wrong");
     ￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼￼STAssertEqualObjects(_testGuy.averageItemLevelEquipped, @355, @"avg item level is wrong");
+    
 
 }
 -(void)testCreateCharacterFromDetailJsonValidateItems

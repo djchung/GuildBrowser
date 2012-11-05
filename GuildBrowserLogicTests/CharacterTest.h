a22f0bc618d0d8950e756499d7ba30fdb1a13c6f
DJ Chung <djchung3@gmail.com>
2012-11-05 10:30:25 -0800

    testing

---
+++ GuildBrowserLogicTests/CharacterTest.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTest.h
+//  GuildBrowser
+//
+//  Created by DJ Chung on 11/1/12.
+//  Copyright (c) 2012 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTest : SenTestCase
+
+@end
